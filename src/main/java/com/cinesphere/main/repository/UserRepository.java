87649f3f7bda1b2421f17e6f7642d86543c25ced
rishi solanki <rishisolanki1709@gmail.com>
2026-01-23 02:28:00 +0530

    feat: add findByEmail method in UserRepository

--- src/main/java/com/cinesphere/main/repository/UserRepository.java
+++ src/main/java/com/cinesphere/main/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.cinesphere.main.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cinesphere.main.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Integer> {
+	Optional<User> findByEmail(String email);
 }
