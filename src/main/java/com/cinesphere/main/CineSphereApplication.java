867512e179f98035d9129748fbc6df8e9aa64a82
rishi solanki <rishisolanki1709@gmail.com>
2026-01-23 01:40:15 +0530

    initialize CineSphere Spring Boot project

---
+++ src/main/java/com/cinesphere/main/CineSphereApplication.java
@@ -0,0 +1,13 @@
+package com.cinesphere.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinesphere/main/CineSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinesphere.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
