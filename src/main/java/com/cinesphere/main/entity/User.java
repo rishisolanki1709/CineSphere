c93ce2963187f64bd5f110b85ce35f6354345d55
rishi solanki <rishisolanki1709@gmail.com>
2026-01-23 02:10:07 +0530

    feat: add User entity for CineSphere

---
+++ src/main/java/com/cinesphere/main/entity/User.java
@@ -0,0 +1,33 @@
+package com.cinesphere.main.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private int id;
+	@Column
+	private String name;
+	@Column
+	private String email;
+	@Column
+	private String password;
+	@Column
+	private String phone;
+	@Column
+	private LocalDateTime createdAt;
+}
