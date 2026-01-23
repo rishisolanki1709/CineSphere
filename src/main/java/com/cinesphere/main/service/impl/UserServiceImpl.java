af0fbe70abdf95566c90a0cf72b95d8943b68d0b
rishi solanki <rishisolanki1709@gmail.com>
2026-01-23 17:31:58 +0530

    added spring security & saved hashed password

--- src/main/java/com/cinesphere/main/service/impl/UserServiceImpl.java
+++ src/main/java/com/cinesphere/main/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cinesphere.main.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.cinesphere.main.customException.EmailAlreadyExistsException;
 import com.cinesphere.main.entity.User;
@@ -12,16 +13,19 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	UserRepository userRepository;
 
+	@Autowired
+	BCryptPasswordEncoder encoder;
+
 	@Override
-	public User registerUser(User user) throws EmailAlreadyExistsException{
+	public User registerUser(User user) throws EmailAlreadyExistsException {
 		try {
 			if (userRepository.existsByEmail(user.getEmail())) {
 				throw new EmailAlreadyExistsException("Email already registered");
 			}
 		} catch (EmailAlreadyExistsException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		user.setPassword(encoder.encode(user.getPassword()));
 		return userRepository.save(user);
 	}
 
