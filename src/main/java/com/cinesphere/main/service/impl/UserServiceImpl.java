11f841505f31e08165e92bb72118079e897baf41
rishi solanki <rishisolanki1709@gmail.com>
2026-01-23 15:42:23 +0530

    created ref : UserRepository

--- src/main/java/com/cinesphere/main/service/impl/UserServiceImpl.java
+++ src/main/java/com/cinesphere/main/service/impl/UserServiceImpl.java
@@ -1,13 +1,18 @@
 package com.cinesphere.main.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.cinesphere.main.entity.User;
+import com.cinesphere.main.repository.UserRepository;
 import com.cinesphere.main.service.UserService;
 
 public class UserServiceImpl implements UserService {
 
+	@Autowired
+	UserRepository userRepository;
+	
 	@Override
 	public User registerUser(User user) {
-		
 		return null;
 	}
 	
