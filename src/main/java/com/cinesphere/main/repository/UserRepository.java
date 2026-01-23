3ec8b26fca0b74e0298255074ec21d69b8f64185
rishi solanki <rishisolanki1709@gmail.com>
2026-01-23 15:32:59 +0530

    added method : existsByEmail()

--- src/main/java/com/cinesphere/main/repository/UserRepository.java
+++ src/main/java/com/cinesphere/main/repository/UserRepository.java
@@ -8,4 +8,6 @@ import com.cinesphere.main.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 	Optional<User> findByEmail(String email);
+
+	boolean existsByEmail(String email);
 }
