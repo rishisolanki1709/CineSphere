98a68d4a15b8f59690c014c219e4dac3ada719b8
rishi solanki <rishisolanki1709@gmail.com>
2026-01-26 14:31:56 +0530

    added date & time functionality

--- src/main/java/com/cinesphere/main/entity/User.java
+++ src/main/java/com/cinesphere/main/entity/User.java
@@ -7,25 +7,37 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 
 @Entity
 @Table
 public class User {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column
 	private int id;
-	@Column
+
+	@Column(nullable = false)
 	private String name;
-	@Column(unique = true)
+
+	@Column(nullable = false, unique = true)
 	private String email;
-	@Column
+
+	@Column(nullable = false)
 	private String password;
-	@Column
+
+	@Column(nullable = false)
 	private String phone;
-	@Column
+
+	@Column(nullable = false)
 	private LocalDateTime createdAt;
+
+	@PrePersist
+	public void onCreate() {
+		this.createdAt = LocalDateTime.now();
+	}
 
 	public int getId() {
 		return id;
